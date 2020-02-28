b8b0eca92f9d2c02d92093619de4321d213f463d
Jaime Ferrando Huertas <jaime.ferrandohuertas@hm.com>
2020-02-28 17:03:28 +0100

    Refactor and added shap feature importance

--- utils/plots.py
+++ utils/plots.py
@@ -1,5 +1,20 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.transforms import Bbox
+
+
+def full_extent(ax, pad=0.0):
+    """Get the full extent of an axes, including axes labels, tick labels, and
+    titles."""
+    # For text objects, we need to draw the figure first, otherwise the extents
+    # are undefined.
+    ax.figure.canvas.draw()
+    items = ax.get_xticklabels() + ax.get_yticklabels() 
+#    items += [ax, ax.title, ax.xaxis.label, ax.yaxis.label]
+    items += [ax, ax.title]
+    bbox = Bbox.union([item.get_window_extent() for item in items])
+
+    return bbox.expanded(1.0 + pad, 1.0 + pad)
 
 def bar_metrics(resultsDict):
     df = pd.DataFrame.from_dict(resultsDict)
@@ -13,25 +28,33 @@ def bar_metrics(resultsDict):
     ax = fig.add_subplot(2,2,1)
     df.loc['mae'].sort_values().plot(kind='bar', colormap='Paired',color=[color_dict.get(x, '#333333') for x in df.loc['mae'].sort_values().index])
     plt.legend()
+    plt.tight_layout()
     plt.title("MAE Metric, lower is better")
-    plt.savefig('results/mae.png')
+    extent = full_extent(ax).transformed(fig.dpi_scale_trans.inverted())
+    plt.savefig('results/mae.png', bbox_inches=extent)
     #rmse plot
     ax = fig.add_subplot(2,2,2)
     df.loc['rmse'].sort_values().plot(kind='bar', colormap='Paired',color=[color_dict.get(x, '#333333') for x in df.loc['rmse'].sort_values().index])
     plt.legend()
+    plt.tight_layout()
     plt.title("RMSE Metric, lower is better")
-    plt.savefig('results/rmse.png')
+    extent = full_extent(ax).transformed(fig.dpi_scale_trans.inverted())
+    plt.savefig('results/rmse.png', bbox_inches=extent)
     #mape plot
     ax = fig.add_subplot(2,2,3)
     df.loc['mape'].sort_values().plot(kind='bar', colormap='Paired',color=[color_dict.get(x, '#333333') for x in df.loc['mape'].sort_values().index])
     plt.legend()
+    plt.tight_layout()
     plt.title("MAPE Metric, lower is better")
-    plt.savefig('results/mape.png')
+    extent = full_extent(ax).transformed(fig.dpi_scale_trans.inverted())
+    plt.savefig('results/mape.png', bbox_inches=extent)
     #r2 plot
     ax = fig.add_subplot(2,2,4)
     df.loc['r2'].sort_values(ascending=False).plot(kind='bar', colormap='Paired',color=[color_dict.get(x, '#333333') for x in df.loc['r2'].sort_values(ascending=False).index])
     plt.legend()
+    plt.tight_layout()
     plt.title("R2 Metric, higher is better")
-    plt.savefig('results/r2.png')
+    extent = full_extent(ax).transformed(fig.dpi_scale_trans.inverted())
+    plt.savefig('results/r2.png', bbox_inches=extent)
     plt.tight_layout()
     plt.show()
